43111b9ce6e3143940cb17634c6ff8ab76c48d27
RaulButuc <raulbutuc@gmail.com>
2015-02-13 21:55:36 +0000

    Added Person class

---
+++ Person/Person.h
@@ -0,0 +1,30 @@
+/**
+	Person.h
+	Purpose: Define the attributes and behaviour of a person in the context of the usage of elevators in a building.
+
+	@author Raul Butuc
+	@version 1.0.0  13/02/2015
+*/
+
+#ifndef PERSON_H
+#define PERSON_H
+
+#include "Building.h"
+#include "Elevator.h"
+
+class Person {
+
+	private:
+		int currentFloor;
+		Building *previousBuilding;
+
+	public:
+		Person(void);
+		void setCurrentFloor(int);
+		int getCurrentFloor(void);
+		void callElevator(Building*, int);
+		~Person(void);
+
+};
+
+#endif
