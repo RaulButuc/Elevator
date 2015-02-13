28a4f62828a47ac073fc244005657ecd09af7818
RaulButuc <raulbutuc@gmail.com>
2015-02-13 21:56:43 +0000

    Updated the Building class

--- Building/Building.cpp
+++ Building/Building.cpp
@@ -1,25 +1,63 @@
 /**
 	Building.cpp
-	Purpose: Describe the behaviour of a building.
+	Purpose: Describe the behaviour of a building in the context of the usage of elevators in a building.
 
 	@author Raul Butuc
-	@version 1.0.0  06/02/2015
+	@version 1.0.1  06/02/2015
 */
 
 #include "Building.h"
 
-Building::Building(int numOfFloors) {
+int Building::uniqueId = 0;
 
+/**
+	Create a building by knowing the number of floors and elevators required.
+
+	@param numOfFloors The number of floors that the building is going to have (accessible to all elevators).
+	@param numOfElevators The number of elevators that the building is going to have.
+*/
+Building::Building(int numOfFloors, int numOfElevators) {
+	id = ++uniqueId;
+	Building::numOfFloors = numOfFloors;
+	initialiseElevators(numOfFloors, numOfElevators);
 }
 
-void Building::addElevator(Elevator *elevator) {
+/**
+	Retrieve the unique id of the building.
 
+	@return The id of the building.
+*/
+int Building::getId(void) {
+	return id;
 }
 
-void Building::removeElevator(Elevator *elevator) {
+/**
+	Choose the closest elevator to pick up the current user.
 
+	@param destinationFloor The destination floor of the elevator's user.
+*/
+void Building::callElevator(int destinationFloor) {
+	// @TODO: Implement the algorithm to choose the best option.
+	// @TODO: As an extra, implement multi-threading, so that more than one elevator can run at the same time.
 }
 
+/**
+	Initialise the list of elevators available within the current building.
+
+	@param numOfFloors The number of floors that the building is going to have (accessible to all elevators).
+	@param numOfElevators The number of elevators that the building is going to have.
+*/
+void Building::initialiseElevators(int numOfFloors, int numOfElevators) {
+	elevators.clear();
+	for (int id = 1; id <= numOfElevators; ++id) {
+		Elevator *elevator = new Elevator(numOfFloors);
+		elevators.push_back(elevator);
+	}
+}
+
+/**
+	Destroy the building and deallocate the memory that is in use.
+*/
 Building::~Building(void) {
-
+	elevators.clear();
 }
--- Building/Building.h
+++ Building/Building.h
@@ -1,9 +1,9 @@
 /**
 	Building.h
-	Purpose: Define the attributes and behaviour of a building.
+	Purpose: Define the attributes and behaviour of a building in the context of the usage of elevators in a building.
 
 	@author Raul Butuc
-	@version 1.0.0  06/02/2015
+	@version 1.0.1  06/02/2015
 */
 
 #ifndef BUILDING_H
@@ -15,14 +15,19 @@
 class Building {
 
 	private:
+		int id;
 		int numOfFloors;
-		std::list<Elevator> elevators;
+		std::list<Elevator*> elevators;
+
+		static int uniqueId;
+
+		void initialiseElevators(int, int);
 
 	public:
-		Building(int);
-		void addElevator(Elevator*);
-		void removeElevator(Elevator*);
-		~Building();
+		Building(int, int);
+		int getId(void);
+		void callElevator(int);
+		~Building(void);
 
 };
 
