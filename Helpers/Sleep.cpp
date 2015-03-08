f8171f3570f691cff8d0f5f921c05a99a7b75b72
Raul Butuc <RaulButuc@users.noreply.github.com>
2015-03-08 23:59:46 +0000

    Updated layout in Sleep.cpp

--- Helpers/Sleep.cpp
+++ Helpers/Sleep.cpp
@@ -1,9 +1,9 @@
 /**
-	Sleep.cpp
-	Purpose: Describe a basic multiplatform sleep function in the context of the usage of elevators in a building.
+    Sleep.cpp
+    Purpose: Describe a basic multiplatform sleep function in the context of the usage of elevators in a building.
 
-	@author Raul Butuc
-	@version 1.0.0  24/01/15
+    @author Raul Butuc
+    @version 1.0.0  24/01/15
 */
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
@@ -13,15 +13,15 @@
 #endif
 
 /**
-	Pauses the program execution for a given amount of milliseconds.
+    Pauses the program execution for a given amount of milliseconds.
 
-	@param milliseconds The number of milliseconds for which the program
-	should pause.
+    @param milliseconds The number of milliseconds for which the program
+    should pause.
 */
 void msleep(int milliseconds) {
-	#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-	Sleep(milliseconds);
-	#else
-	usleep(static_cast<useconds_t>(milliseconds)* 1000);
-	#endif
+    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+    Sleep(milliseconds);
+    #else
+    usleep(static_cast<useconds_t>(milliseconds)* 1000);
+    #endif
 }
