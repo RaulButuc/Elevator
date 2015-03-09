a9e980581ba3ac7ec2cd65718b653ced31fc1f82
Raul Butuc <RaulButuc@users.noreply.github.com>
2015-03-09 00:00:15 +0000

    Updated layout in Sleep.h

--- Helpers/Sleep.h
+++ Helpers/Sleep.h
@@ -1,9 +1,9 @@
 /**
-	Sleep.h
-	Purpose: Define a basic multiplatform sleep function in the context of the usage of elevators in a building.
+    Sleep.h
+    Purpose: Define a basic multiplatform sleep function in the context of the usage of elevators in a building.
 
-	@author Raul Butuc
-	@version 1.0.0  24/01/15
+    @author Raul Butuc
+    @version 1.0.0  24/01/15
 */
 
 #ifndef SLEEP_H
